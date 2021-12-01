de37f0bb6ac82bedd078174e833cf6f74ba19cea
SIDDHANT_KUMAR <proteanox56@gmail.com>
2021-12-01 16:23:57 +0530

    list basic exercise version 2.0

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -5,6 +5,10 @@ def main():
         numbers = float(input("Number: "))
         num_list.append(numbers)
 
+    output_statements(iterations, num_list)
+
+
+def output_statements(iterations, num_list):
     print(
         f"The first number is {num_list[0]} \nThe last number is {num_list[len(num_list) - 1]} \n The smallest number "
         f"is {min(num_list)}\nThe largest "
