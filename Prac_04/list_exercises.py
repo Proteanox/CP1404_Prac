d494a916d05c6a799a42b73438e24b188bb78567
SIDDHANT_KUMAR <proteanox56@gmail.com>
2021-12-01 16:23:01 +0530

    list basic exercise version 1.0

---
+++ Prac_04/list_exercises.py
@@ -0,0 +1,14 @@
+def main():
+    iterations = int(input("Enter no of iterations: "))
+    num_list = []
+    for i in range(iterations):
+        numbers = float(input("Number: "))
+        num_list.append(numbers)
+
+    print(
+        f"The first number is {num_list[0]} \nThe last number is {num_list[len(num_list) - 1]} \n The smallest number "
+        f"is {min(num_list)}\nThe largest "
+        f"number is {max(num_list)}\nThe average of the numbers is {sum(num_list) / iterations}")
+
+
+main()
