af3755921ebcfcb6df3e331b9303823c0c1962e4
SIDDHANT_KUMAR <proteanox56@gmail.com>
2021-12-01 15:16:43 +0530

    Subject_reader py formatted and added with statement output function

---
+++ Prac_04/subject_reader.py
@@ -0,0 +1,38 @@
+"""
+CP1404/CP5632 Practical
+Data file -> lists program
+"""
+
+FILENAME = "subject_data.txt"
+
+
+def main():
+    data = get_data()
+    display_teachers(data)
+
+
+def get_data():
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
+    input_file = open(FILENAME)
+    for line in input_file:
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(parts)  # See if that worked
+        data.append(parts)
+        print("----------")
+    input_file.close()
+    return data
+
+
+def display_teachers(data):
+    """ print the subject being taught by the teacher in succession """
+    for subject_info in data:
+        print(f"{subject_info[0]} is being taught by {subject_info[1]} to {subject_info[2]} students.")
+
+
+main()
