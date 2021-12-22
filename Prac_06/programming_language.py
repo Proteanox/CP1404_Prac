6a0e6d277cdb1772539ae75c0a51ff916f8a541c
SIDDHANT_KUMAR <proteanox56@gmail.com>
2021-12-22 20:58:50 +0530

    programming_languages single commit with list correction and orientation

--- Prac_06/programming_language.py
+++ Prac_06/programming_language.py
@@ -12,13 +12,3 @@ class ProgrammingLanguage:
         return self.typing == "Dynamic"
 
 
-def language():
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-
-    languages = [ruby, python, visual_basic]
-
-    for i in languages:
-        if i.is_dynamic():
-            print(i.name)
