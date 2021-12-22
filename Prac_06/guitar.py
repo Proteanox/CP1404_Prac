c79e57a3e571d7bfc90b33840dc8da2d43095532
SIDDHANT_KUMAR <proteanox56@gmail.com>
2021-12-22 21:41:21 +0530

    guitar class program

---
+++ Prac_06/guitar.py
@@ -0,0 +1,21 @@
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} {self.year} : {self.cost}"
+
+    def get_age(self):
+        YEAR = 2020
+        year_diff = YEAR - self.year
+        return year_diff
+
+    def is_vintage(self):
+        YEAR = 2020
+        year_diff = YEAR - self.year
+        if year_diff >= 50:
+            return "True"
+        else:
+            return "False"
--- Prac_06/languages.py
+++ Prac_06/languages.py
@@ -1,4 +1,15 @@
-from Prac_06.programming_language import ProgrammingLanguage
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection="", year=""):
+        self.year = year
+        self.reflection = reflection
+        self.typing = typing
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name}, Typing: {self.typing} , Reflection: {self.reflection}, in Year: {self.year}"
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
 
 
 def language():
