c3910fb946e6761ee481bdc59611643a6d348119
SIDDHANT_KUMAR <proteanox56@gmail.com>
2022-01-21 20:41:20 +0530

    sort files 2 part 1

--- Prac_09/sort_files_2.py
+++ Prac_09/sort_files_2.py
@@ -9,3 +9,15 @@ def main():
             continue
 
         end = file.split('.')[-1]
+        if end not in extension_to_category:
+            category = input(f"Input Your Categorization Scale {end}")
+            extension_to_category[end] = category
+            try:
+                # We don't expect to get an exception due to the if statement
+                os.mkdir(category)
+            except FileExistsError:
+                pass
+
+        os.rename(file, f"{file}/{end}")
+
+main()
